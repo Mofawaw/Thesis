0c69227259d6ac4bce3fe3a2766396a395435b98
Kai Zheng <kaizhengde@gmail.com>
2023-12-28 19:22:47 +0800

    setup custom colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold">
+      <h1 className="text-3xl font-bold text-th-black-100">
         Hello world!
       </h1>
     </>
