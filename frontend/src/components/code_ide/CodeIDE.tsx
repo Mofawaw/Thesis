39d0b82cc7ecf9fc47a9028a9ce8f5a386528797
Kai Zheng <kaizhengde@gmail.com>
2023-12-29 19:22:28 +0800

    version 2.3 codeide (output component)

--- frontend/src/components/code_ide/CodeIDE.tsx
+++ frontend/src/components/code_ide/CodeIDE.tsx
@@ -3,6 +3,8 @@ import useCodeIDEStore from './codeide_store.ts'
 
 export default function CodeIDE({ height }: { height: number }) {
   const code = useCodeIDEStore((state) => state.code)
+  const output = useCodeIDEStore((state) => state.output)
+  const setOutput = useCodeIDEStore((state) => state.setOutput)
 
   function compile() {
     fetch('http://127.0.0.1:5000/compile', {
@@ -12,8 +14,14 @@ export default function CodeIDE({ height }: { height: number }) {
       },
       body: JSON.stringify({ code: code }),
     })
-      .then(response => response.json())
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
       .then(data => {
+        setOutput(data.output);
         console.log('Output:', data.output);
       })
       .catch((error) => {
@@ -22,10 +30,12 @@ export default function CodeIDE({ height }: { height: number }) {
   }
 
   return (
-    <div className="flex flex-col gap-0 p-4">
-      <CodeEditor height={height - 200} />
+    <div className="flex flex-col gap-2 p-4">
+      <div className="my-2">
+        <CodeEditor height={height - 200} />
+      </div>
       <hr className="h-px my-0 bg-th-black-20 border-0" />
-      <div className="flex flex-row justify-between py-2">
+      <div className="flex flex-row justify-between">
         <button className="text-th-black-100" onClick={compile}>Run</button>
         <div className="flex flex-row gap-4">
           <button className="text-th-tint-100">Back</button>
@@ -33,7 +43,7 @@ export default function CodeIDE({ height }: { height: number }) {
         </div>
       </div>
       <hr className="h-px my-0 bg-th-black-20 border-0" />
+      <pre>{output}</pre>
     </div>
   )
-
 }
--- frontend/src/components/code_ide/codeide_store.ts
+++ frontend/src/components/code_ide/codeide_store.ts
@@ -2,12 +2,16 @@ import { create } from 'zustand';
 
 type CodeIDEStore = {
   code: string;
+  output: string;
   setCode: (newCode: string) => (void);
+  setOutput: (newOutput: string) => (void);
 };
 
 const useCodeIDEStore = create<CodeIDEStore>((set) => ({
   code: '',
-  setCode: (newCode: string) => set(() => ({ code: newCode }))
+  output: '',
+  setCode: (newCode: string) => set(() => ({ code: newCode })),
+  setOutput: (newOutput: string) => set(() => ({ output: newOutput }))
 }));
 
 export default useCodeIDEStore;
