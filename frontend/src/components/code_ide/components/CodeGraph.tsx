0de597eae7696df949a8e9b6051472d63e0cf0f7
Kai Zheng <kaizhengde@gmail.com>
2023-12-31 07:52:12 +0800

    version 2 codegraph (konva)

--- frontend/src/components/code_ide/components/CodeGraph.tsx
+++ frontend/src/components/code_ide/components/CodeGraph.tsx
@@ -1,7 +1,39 @@
+import { useRef, useEffect, useState } from 'react';
+import { Stage, Layer, Rect } from 'react-konva';
 import useCodeIDEStore from '../codeide_store.ts'
 
 export default function CodeGraph() {
+  const canvasRef = useRef<HTMLDivElement>(null);
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    const updateSize = () => {
+      if (canvasRef.current) {
+        setDimensions({
+          width: canvasRef.current.offsetWidth,
+          height: canvasRef.current.offsetHeight,
+        });
+      }
+    };
+    updateSize();
+    window.addEventListener('resize', updateSize);
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+
   return (
-    <div></div>
-  )
+    <div ref={canvasRef} className="w-full h-full nodrag nowheel">
+      <Stage width={dimensions.width} height={dimensions.height}>
+        <Layer>
+          <Rect
+            x={20}
+            y={20}
+            width={100}
+            height={100}
+            fill="red"
+            draggable
+          />
+        </Layer>
+      </Stage>
+    </div>
+  );
 }
