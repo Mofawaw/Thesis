7c13d4d4c57363262f7036bbffe3a25d99054c4d
Kai Zheng <kaizhengde@gmail.com>
2023-12-31 08:09:46 +0800

    version 2 codegraph (basic flow example)

--- frontend/src/components/code_ide/components/CodeGraph.tsx
+++ frontend/src/components/code_ide/components/CodeGraph.tsx
@@ -1,10 +1,24 @@
 import { useRef, useEffect, useState } from 'react';
-import { Stage, Layer, Rect } from 'react-konva';
+import { Stage, Layer, Rect, Arrow } from 'react-konva';
 import useCodeIDEStore from '../codeide_store.ts'
 
 export default function CodeGraph() {
   const canvasRef = useRef<HTMLDivElement>(null);
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+  // Positions for rectangles
+  const rect1 = { x: 50, y: 75, width: 100, height: 50 };
+  const rect2 = { x: 200, y: 75, width: 100, height: 50 };
+
+  // Calculate the start and end points for the arrow
+  const arrowStart = {
+    x: rect1.x + rect1.width,
+    y: rect1.y + rect1.height / 2,
+  };
+  const arrowEnd = {
+    x: rect2.x,
+    y: rect2.y + rect2.height / 2,
+  };
 
   useEffect(() => {
     const updateSize = () => {
@@ -24,13 +38,17 @@ export default function CodeGraph() {
     <div ref={canvasRef} className="w-full h-full nodrag nowheel">
       <Stage width={dimensions.width} height={dimensions.height}>
         <Layer>
-          <Rect
-            x={20}
-            y={20}
-            width={100}
-            height={100}
-            fill="red"
-            draggable
+          <Rect {...rect1} fill="green" draggable />
+
+          <Rect {...rect2} fill="blue" draggable />
+
+          <Arrow
+            points={[arrowStart.x, arrowStart.y, arrowEnd.x, arrowEnd.y]}
+            pointerLength={10}
+            pointerWidth={10}
+            fill="black"
+            stroke="black"
+            strokeWidth={2}
           />
         </Layer>
       </Stage>
