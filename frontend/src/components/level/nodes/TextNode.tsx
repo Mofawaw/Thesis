fc70028a8088fd335345d00e477ba921b7fcde21
Kai Zheng <kaizhengde@gmail.com>
2024-01-09 09:57:40 +0800

    version 7.5 codeide (text scrollability)

--- frontend/src/components/level/nodes/TextNode.tsx
+++ frontend/src/components/level/nodes/TextNode.tsx
@@ -1,11 +1,26 @@
-import { TextNodeData, ThNodeSize } from '../types/ThTypes';
+import { useState } from 'react';
+import { TextNodeData } from '../types/ThTypes';
 import ComponentNode from './component_node/ComponentNode';
 
 export default function TextNode({ data }: { data: TextNodeData }) {
+  // Layout
+  const [size, setSize] = useState({ width: 0, height: 0 });
+
+  const handleSizeChange = (size: any) => {
+    setSize(size);
+  };
+
+  // Node Header
+  const headerHeight = 60;
+
   return (
-    <ComponentNode data={data} maxWidth={data.width * 3.2} minHeight={data.height * 0.4}>
+    <ComponentNode data={data} maxWidth={data.width * 3.2} minHeight={data.height * 0.4} onSizeChange={handleSizeChange}>
       <h3 className="px-4 pt-8">{data.title ?? ""}</h3>
-      <p className="p-4">{data.text.description}</p>
+      <div className="overflow-hidden p-4">
+        <div className="nowheel" style={{ height: `${size.height - headerHeight - 55}px`, overflow: 'auto' }} >
+          <p>{data.text.description}</p>
+        </div>
+      </div>
     </ComponentNode>
   );
 }
