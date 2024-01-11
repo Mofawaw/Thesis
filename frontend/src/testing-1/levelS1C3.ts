b0e2ed76169e1bc27d9633d7508673c657e16e3e
Kai Zheng <kaizhengde@gmail.com>
2024-01-11 12:00:59 +0800

    improved testing logic

---
+++ frontend/src/testing-1/levelS1C3.ts
@@ -0,0 +1,14 @@
+//   {
+//     id: "s-3-l-1",
+//     stage: stages[2],
+//     category: categories[2],
+//     label: "Level 3.1",
+//     nodes: [
+//       { node: categories[2].nodes[0], data: { codeIDE: { initialCode: c3_initialCode } } },
+//       { node: categories[2].nodes[1], data: { codeIDE: { initialGraph: c3_initialGraph } } },
+//       { node: categories[2].nodes[2], data: { text: { description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." } } }
+//     ],
+//     tippNodes: [],
+//     expected: { graph: c3_expectedGraph }
+//   },
+// ]
--- frontend/src/testing-1/testingStore.ts
+++ frontend/src/testing-1/testingStore.ts
@@ -2,31 +2,35 @@ import { create } from 'zustand';
 import { ThLevel } from '../components/level/types/thTypes';
 
 import levelS1C1 from './levelS1C1';
+import levelS1C2 from './levelS1C2';
 
 export type TestingStore = {
   currentLevel: ThLevel;
-  // setCurrentLevel: (stage: "s1" | "s2" | "s3", category: "c1" | "c2" | "c3") => void;
-  nextLevel: (currentLevelId: string) => void;
+  nextLevel: () => void;
 };
 
 const useTestingStore = create<TestingStore>((set) => ({
   currentLevel: levelS1C1,
 
-  nextLevel: (currentLevelId) => () => {
-    switch (currentLevelId) {
-      // case "l-s1c1": set({ currentLevel: levelS1C2 }); break;
-      // case "l-s1c2": set({ currentLevel: levelS1C3 }); break;
-      // case "l-s1c3": set({ currentLevel: levelS2C1 }); break;
-      // case "l-s2c1": set({ currentLevel: levelS2C2 }); break;
-      // case "l-s2c2": set({ currentLevel: levelS2C3 }); break;
-      // case "l-s2c3": set({ currentLevel: levelS3C1 }); break;
-      // case "l-s3c1": set({ currentLevel: levelS3C2 }); break;
-      // case "l-s3c2": set({ currentLevel: levelS3C3 }); break;
-      // case "l-s3c3": set({ currentLevel: levelS1C1 }); break;
-      default:
-        set({ currentLevel: levelS1C1 });
-    }
+  nextLevel: () => {
+    set((state) => {
+      switch (state.currentLevel.id) {
+        case "l-s1c1": return { currentLevel: levelS1C2 };
+        // These cases are commented out as the levels are yet to be created and imported.
+        // case "l-s1c2": return { currentLevel: levelS1C3 };
+        // case "l-s1c3": return { currentLevel: levelS2C1 };
+        // case "l-s2c1": return { currentLevel: levelS2C2 };
+        // case "l-s2c2": return { currentLevel: levelS2C3 };
+        // case "l-s2c3": return { currentLevel: levelS3C1 };
+        // case "l-s3c1": return { currentLevel: levelS3C2 };
+        // case "l-s3c2": return { currentLevel: levelS3C3 };
+        // case "l-s3c3": return { currentLevel: levelS1C1 };
+        default:
+          return { currentLevel: levelS1C1 };
+      }
+    });
   },
 }));
 
 export default useTestingStore;
+
