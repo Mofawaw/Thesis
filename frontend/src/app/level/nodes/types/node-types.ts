8a793eb2b809593d3b265f3f6aaf1dc2f9a84d06
Kai Zheng <kaizhengde@gmail.com>
2024-02-12 12:56:05 +0100

    version 11.1 level (improved layout)

--- frontend/src/app/level/nodes/types/node-types.ts
+++ frontend/src/app/level/nodes/types/node-types.ts
@@ -8,9 +8,9 @@ import { ThColorKey } from "@/utilities/th-color";
 export const nodeTypes = { codeIDE: CodeIDENode, text: TextNode, tutorial: TutorialNode };
 
 export class ThNodeSize {
-  static small = { width: 500, height: 800 };
-  static medium = { width: 700, height: 800 };
-  static large = { width: 1200, height: 800 };
+  static small = { width: 500, height: 750 };
+  static medium = { width: 700, height: 750 };
+  static large = { width: 1100, height: 750 };
 
   static fromString(sizeType: string) {
     switch (sizeType) {
